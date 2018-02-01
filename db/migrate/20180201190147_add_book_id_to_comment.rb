50b14bec6df15f8b3f59ceefc98027e7467734e7
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-02-01 11:50:26 -0800

    test

--- db/migrate/20180201190147_add_book_id_to_comment.rb
+++
@@ -1,6 +0,0 @@
-class AddBookIdToComment < ActiveRecord::Migration[5.0]
-  def change
-    add_column :comments, :book_id, :string
-    add_column :comments, :content, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201190004) do
+ActiveRecord::Schema.define(version: 20180119162256) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -18,11 +18,6 @@ ActiveRecord::Schema.define(version: 20180201190004) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
