035c01eb3b8ba77288a2b2e332c0aec29bf638bb
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-02-01 07:40:45 -0800

    cors added

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module BookApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :patch, :options]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
