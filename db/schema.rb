cb0e49f681025a0f09693286141dead7d709860f
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-02-01 11:47:42 -0800

    removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201190147) do
+ActiveRecord::Schema.define(version: 20180201190004) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -23,8 +23,6 @@ ActiveRecord::Schema.define(version: 20180201190147) do
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "book_id"
-    t.string   "content"
   end
 
 end
